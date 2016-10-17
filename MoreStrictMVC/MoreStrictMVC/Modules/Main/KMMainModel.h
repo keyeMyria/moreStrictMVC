b3c1e2e3ac46ab24399a64ddf0a20ece5e0dcf0c
whello <wankeye@foxmail.com>
2016-10-17 22:45:19 +0800

    main

---
+++ MoreStrictMVC/MoreStrictMVC/Modules/Main/KMMainModel.h
@@ -0,0 +1,13 @@
+//
+//  KMMainModel.h
+//  MoreStrictMVC
+//
+//  Created by Keye Myria on 10/17/16.
+//  Copyright © 2016 Keye Myria. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KMMainModel : NSObject
+
+@end
---
+++ MoreStrictMVC/MoreStrictMVC/Modules/Main/KMMainModel.m
@@ -0,0 +1,13 @@
+//
+//  KMMainModel.m
+//  MoreStrictMVC
+//
+//  Created by Keye Myria on 10/17/16.
+//  Copyright © 2016 Keye Myria. All rights reserved.
+//
+
+#import "KMMainModel.h"
+
+@implementation KMMainModel
+
+@end
