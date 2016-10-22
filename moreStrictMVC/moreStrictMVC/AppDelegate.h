2641d68a8aa5773b88e7bed75e5cef6767d6069b
whello <wankeye@foxmail.com>
2016-10-23 07:57:11 +0800

    ns

---
+++ moreStrictMVC/moreStrictMVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  moreStrictMVC
+//
+//  Created by Keye Myria on 10/23/16.
+//  Copyright © 2016 Keye Myria. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ moreStrictMVC/moreStrictMVC/AppDelegate.m
@@ -0,0 +1,53 @@
+//
+//  AppDelegate.m
+//  moreStrictMVC
+//
+//  Created by Keye Myria on 10/23/16.
+//  Copyright © 2016 Keye Myria. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    
+    
+    
+    
+    return YES;
+}
+
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    
+}
+
+
+@end
